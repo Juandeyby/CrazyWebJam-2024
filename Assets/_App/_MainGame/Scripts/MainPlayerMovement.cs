bbf56a6ee602e1e4da97c00bec0f0b7976076938
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2024-11-02 15:57:09 -0500

    feat(tamagotchi): add mouse movement

--- Assets/_App/_MainGame/Scripts/MainPlayerMovement.cs
+++ Assets/_App/_MainGame/Scripts/MainPlayerMovement.cs
@@ -1,33 +1,65 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MainMovementPlayer : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _rotationSpeed = 750f;
+    [SerializeField] private LayerMask _groundLayer;
     private Rigidbody _rb;
+    private NavMeshAgent _navMeshAgent;
     private Vector3 _movementInput;
     private Transform _mainCameraTransform;
+    private Camera _mainCamera;
+    private bool _isMoving;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
         if (Camera.main == null)
         {
             throw new Exception("Main camera not found");
         }
         _mainCameraTransform = Camera.main.transform;
+        _mainCamera = Camera.main;
     }
 
     private void Update()
     {
+        KeyboardMovement();
+        MouseMovement();
+        
+        RotateMovementToCamera();
+        RotateLookForward();
+    }
+    
+    private void KeyboardMovement()
+    {
         var moveHorizontal = Input.GetAxis("Horizontal");
         var moveVertical = Input.GetAxis("Vertical");
         
+        _isMoving = moveHorizontal != 0 || moveVertical != 0;
+        if (_isMoving)
+        {
+            _navMeshAgent.ResetPath();
+        }
+        
         _movementInput = new Vector3(moveHorizontal, 0f, moveVertical);
-
-        RotateMovementToCamera();
-        RotateLookForward();
+    }
+    
+    private void MouseMovement()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, _groundLayer))
+            {
+                var targetPosition = hit.point;
+                _navMeshAgent.SetDestination(targetPosition);
+            }
+        }
     }
 
     private void FixedUpdate()

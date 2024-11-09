2b1dcb47803ec081617304a43644e714427f3f0c
Juan Deyby Carlos-Chullo <deyby57@gmail.com>
2024-11-08 19:02:25 -0500

    feat(items): get item attributes for all types of items

--- Assets/_App/_MainGame/Scripts/Item/View/ItemStoreButton.cs
+++ Assets/_App/_MainGame/Scripts/Item/View/ItemStoreButton.cs
@@ -1,9 +1,13 @@
 using System;
 using Lean.Gui;
+using TMPro;
 using UnityEngine;
 
 public class ItemStoreButton : MonoBehaviour
 {
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _priceText;
     private LeanButton _button;
     private Item _item;
 
@@ -12,6 +16,8 @@ public class ItemStoreButton : MonoBehaviour
         _button = GetComponent<LeanButton>();
         _item = GetComponent<Item>();
         _button.OnClick.AddListener(OnButtonClick);
+        
+        SetItem(_item.ItemId);
     }
 
     private void OnButtonClick()
@@ -22,5 +28,12 @@ public class ItemStoreButton : MonoBehaviour
     public void SetAvailability(bool isAvailable)
     {
         _button.interactable = isAvailable;
+    }
+    
+    private void SetItem(ItemId itemId)
+    {
+        _icon = itemId.Icon;
+        _nameText.text = itemId.Name;
+        _priceText.text = itemId.Price.ToString();
     }
 }
